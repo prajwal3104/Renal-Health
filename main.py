9614af4040f364744e945bb441886c3b33b2b316
prajwal3104 <Prajwal0836@gmail.com>
2024-03-10 03:10:51 +0530

    prepare base model updated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.renalClassifier import logger
 from src.renalClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from src.renalClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,3 +13,13 @@ except Exception as e:
     logger.error(e)
     raise e
 
+STAGE_NAME = "Stage 02: Prepare Base Model Stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
